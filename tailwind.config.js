df8071c2ce63852bfd4a883332db5d91aaa7dd98
Plhume <72684309+Plhume@users.noreply.github.com>
2024-12-01 11:35:00 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./server/pages/**/*.ejs",
+    "./server/public/assets/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
